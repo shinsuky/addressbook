b0ed2e70a7434608d96cbdb2caa2bf24abbb8c97
shinsuky <shinsuke@ieee.org>
2019-10-09 13:56:05 +0900

    第2版に修正 コマンドを追加

--- denden.c
+++ denden.c
@@ -1,13 +1,13 @@
 /*	クオリティＣプログラミング　中級編
 **
-**	例題1：
-**		住所録管理表・第1版
+**	例題2：
+**		住所録管理表・第2版
 **
-**		file name：	denden1.c
+**		file name：	denden2.c
 **		コンパイルする場合、ライブラリのリンクオプション　-lncurses をつけること
 **
 **		composed by Naohiko Takeda
-**					ver. 1.00.	1990.5.27.
+**					ver. 1.00.	1990.5.30.
 **		composed by Yoshihide Chubachi
 **					ver. 1.10.	2003.4.27.
 **		composed by Shinsuke Kobayashi
@@ -24,6 +24,7 @@
 #include	<ctype.h>
 #include	<ncurses.h>
 #include 	<locale.h>
+#include  <stdlib.h>
 
 /*---------------------*/
 /*	システム定数の宣言 */
@@ -48,19 +49,29 @@
 #define		UP					1		/* 前ページ */
 #define		DOWN				0		/* 次ページ */
 
+#define		FNAME_MAX			128		/* ファイル名の最大長さ */
+
 /* コマンドの定義 */
 
-#define		CMD_MAX				4		/* 第1版 */
+#define		CMD_MAX				8		/* 第2版 */
 
 #define		APPEND				'A'		/* データの追加 */
+#define		BEFORE				'B'		/* 前のデータへ */
+#define		NEXT				'N'		/* 次のデータへ */
 #define		UP_PAGE				'U'		/* 前ページへ */
 #define		DOWN_PAGE			'D'		/* 次ページへ */
+#define		LOAD				'L'		/* データのロード */
+#define		SAVE				'S'		/* データのセーブ */
 #define		QUIT				'Q'		/* 終了 */
 
 char	cmd_list[] = {
 			APPEND,
+			BEFORE,
+			NEXT,
 			UP_PAGE,
 			DOWN_PAGE,
+			LOAD,
+			SAVE,
 			QUIT
 		};
 
@@ -78,6 +89,7 @@ char	address[RECORD_MAX][ADDR_LEN];	/* 住所 */
 char	tel_no[RECORD_MAX][TEL_LEN];	/* 電話番号 */
 char	zip[RECORD_MAX][ZIP_LEN];		/* 郵便番号 */
 
+char	data_file[FNAME_MAX];			/* データファイル名 */
 
 /*---------------------*/
 /*	使用する関数の宣言 */
@@ -90,6 +102,9 @@ void	exec_cmd( char cmd );
 /* 住所録管理コマンド */
 void	data_append( void );
 void	paging( int direction );
+void	cur_move( int direction );
+void	data_load( void );
+void	data_save( void );
 
 void	init_buff();
 
@@ -97,7 +112,8 @@ void	init_buff();
 void	init_disp( void );
 void	show_one_page( void );
 void	show_record( int num, int pos );
-
+void	del_lines( int start, int end );
+void	disp_err( char *msg );
 
 
 /* main */
@@ -115,6 +131,8 @@ int	main()
 	/* コマンドの入力と実行 */
 	while(( command = input_cmd() ) != QUIT ){
 		exec_cmd( command );
+		/* データの再表示 */
+		show_one_page();
 	}
 
 	/* 後始末する */
@@ -132,7 +150,7 @@ int	main()
 #define		CMD_ERR_LINE	MSG_LINE+1
 #define		CMD_LINE		MSG_LINE+2
 
-#define		CMD_MSG			"コマンド(A:追加 U:次頁 D:前頁 Q:終了)"
+#define		CMD_MSG			"コマンド(A:追加 B:前 N:後 U:次頁 D:前頁 L:ロード S:セーブ Q:終了)"
 #define		CMD_ERR_MSG		"不適当なコマンドでした"
 #define		CMD_PROMPT		" >>"
 
@@ -161,7 +179,7 @@ char	input_cmd()
 
 	/* コマンドの入力 */
 	for( ; ; ){
-		move(CMD_LINE, 1);		/* コマンドを1文字入力する */
+		move(CMD_LINE, 1);	/* コマンドを1文字入力する */
 		clrtoeol();
 		printw( CMD_PROMPT );
 		getstr( buffer );
@@ -181,11 +199,7 @@ char	input_cmd()
 	}
 
 	/* 後始末 */
-	move(CMD_MSG_LINE, 1);
-	for( i = CMD_MSG_LINE ; i<= CMD_LINE ; i++ ){
-		clrtoeol();
-		addch('\n');
-	}
+	del_lines( CMD_MSG_LINE, CMD_LINE );
 	return( cmd );
 }
 
@@ -214,13 +228,22 @@ void exec_cmd(
 	case	DOWN_PAGE:
 		paging( DOWN );		/* 前ページ */
 		break;
+	case	NEXT:
+		cur_move( UP );		/* 次のデータ */
+		break;
+	case	BEFORE:
+		cur_move( DOWN );	/* 前のデータ */
+		break;
+	case	LOAD:
+		data_load();		/* データのロード */
+		break;
+	case	SAVE:
+		data_save();		/* データのセーブ */
+		break;
 	default:
 		break;
 	}
-	/* データの再表示 */
-	show_one_page();
 }
-
 
 /*-----------------------*/
 /*	住所録管理コマンド群 */
@@ -229,7 +252,10 @@ void exec_cmd(
 #define		APPEND_LINE		MSG_LINE
 #define		APPEND_END_LINE	MSG_LINE+4
 
+#define		FBUF_MAX		200
+
 #define		ERR_APPEND		"データが一杯で，もう追加できません．"
+#define		ERR_FILE		"ファイルがオープンできません．"
 
 #define		WAIT_MSG		"確認したら，好きなキーを押して下さい"
 
@@ -247,15 +273,12 @@ void exec_cmd(
 void	data_append( void )
 {
 	int		i;
-	char	buffer[BUFFER_LEN];
 
 	move(APPEND_LINE, 1);
 
 	/* データが一杯かどうか調べる */
 	if( rec_qty >= RECORD_MAX ){
-		printw( ERR_APPEND );
-		printw( WAIT_MSG );
-		getstr( buffer );
+		disp_err( ERR_APPEND );
 		return;
 	}
 
@@ -274,11 +297,7 @@ void	data_append( void )
 	cursor = ++rec_qty;
 
 	/* 後始末 */
-	move(APPEND_LINE, 1);
-	for( i = APPEND_LINE ; i <= APPEND_END_LINE ; i++ ){
-		clrtoeol();
-		addch('\n');
-	}
+	del_lines( APPEND_LINE, APPEND_END_LINE );
 }
 
 
@@ -316,6 +335,161 @@ void paging(
 		break;
 	}
 	cursor = next;
+}
+
+
+/* cur_move */
+/*----------------------------------------------------*/
+/*
+	書式：	void	cur_move( int direction )
+
+	機能：	注目行を前，あるいは次データに移動する．移動できない時は，そのまま．
+
+	返値：	なし
+*/
+
+void	cur_move(
+	int	direction	/* 注目行の移動方向 */
+)
+{
+	int		next;
+
+	if( rec_qty == 0 )	/* データなし */
+		return;
+
+	switch( direction ){
+	case	UP:
+		next = cursor + 1;
+		if( next > rec_qty )
+			next = rec_qty;
+		break;
+	case	DOWN:
+		next = cursor - 1;
+		if( next <= 0 )
+			next = 1;
+		break;
+	default:
+		break;
+	}
+	cursor = next;
+}
+
+
+/* data_load */
+/*----------------------------------------------------*/
+/*
+	書式：	void	data_load( void )
+
+	機能：	住所録データを，指定されたファイルからロードする．
+
+	返値：	なし
+*/
+
+void	data_load( void )
+{
+	char	buffer[FBUF_MAX];
+	FILE	*fp;
+	wchar_t   name[NAME_LEN], kana[NAME_LEN], zip_tmp[ZIP_LEN], addr_tmp[ADDR_LEN], tel_tmp[TEL_LEN];
+
+	/* ファイル名の入力 */
+	move(MSG_LINE, 1);
+	printw("ファイルをロードします");
+	move(CMD_LINE, 1);
+	printw("ファイル名を入力してください >>");
+	getstr( data_file );
+
+	/* データファイルからの読み込み */
+	if( (fp=fopen( data_file, "r" )) == NULL){
+		disp_err( ERR_FILE );
+		return;
+	}
+
+	rec_qty = 0;
+	printw("ロード中");
+	while( fgets( buffer, FBUF_MAX, fp ) != NULL ){
+		addch('*');
+		//sscanf( buffer, "%s %s %s %s %s", j_name[rec_qty], k_name[rec_qty], zip[rec_qty], address[rec_qty], tel_no[rec_qty] );
+		//
+		//  元は上記のコードだけだったが、UTF-8 を扱う場合に、char で読み込むと、
+		//  文字によってスペースと誤って認識されてしまうケースで文字化けが起こっていた。
+		//  その問題に対処するため、ワイド文字列で読み込み、マルチバイト文字に変換して記録することとした。
+		sscanf( buffer, "%S %S %S %S %S", name, kana, zip_tmp, addr_tmp, tel_tmp );
+		wcstombs(j_name[rec_qty], name, sizeof(name));
+		wcstombs(k_name[rec_qty], kana, sizeof(kana));
+		wcstombs(zip[rec_qty], zip_tmp, sizeof(zip_tmp));
+		wcstombs(address[rec_qty], addr_tmp, sizeof(addr_tmp));
+		wcstombs(tel_no[rec_qty], tel_tmp, sizeof(tel_tmp));
+
+		rec_qty++;
+	}
+
+	/* 後始末 */
+	fclose( fp );
+	cursor = 1;
+	del_lines( MSG_LINE, CMD_LINE+1 );
+}
+
+
+/* data_save */
+/*----------------------------------------------------*/
+/*
+	書式：	void	data_save( void )
+
+	機能：	住所録データを，指定されたファイルにセーブする．
+
+	返値：	なし
+*/
+
+void	data_save( void )
+{
+	int		i, end;
+	FILE	*fp;
+
+	/* ファイル名の入力 */
+	move(MSG_LINE, 1);
+	printw("ファイルをセーブします");
+	move(CMD_LINE, 1);
+	printw("ファイル名を入力してください >>" );
+	getstr( data_file );
+
+	/* データファイルへの書き出し */
+	if( ( fp=fopen( data_file, "w" ) ) == NULL){
+		disp_err( ERR_FILE );
+		return;
+	}
+	end = rec_qty-1;
+	printw("セーブ中");
+	for( i=0 ; i<=end ; i++ ){
+		fprintf( fp, "%s %s %s %s %s\n", j_name[i], k_name[i], zip[i], address[i], tel_no[i] );
+	}
+
+	/* 後始末 */
+	fclose( fp );
+	del_lines( MSG_LINE, CMD_LINE+1 );
+}
+
+
+/* disp_err */
+/*----------------------------------------------------*/
+/*
+	書式：	void	disp_err( char *msg )
+
+	機能：	引数で渡されたエラーメッセージを表示し，なにかキーが押されるまで待つ．
+			キーが押されたら，画面のメッセージ領域をクリアし，戻る．
+
+	返値：	なし
+*/
+
+void	disp_err(
+	char	*msg /* エラーメッセージ */
+)
+{
+	char	buffer[BUFFER_LEN];
+
+	printw( msg );
+	printw( WAIT_MSG );
+	getstr( buffer );
+	del_lines( APPEND_LINE, APPEND_END_LINE );
 }
 
 /* init_buff */
@@ -367,8 +541,8 @@ void init_disp( void )
 	setlocale(LC_ALL,"");			//  for UTF-8 setting
 	initscr();								//  画面初期化
 	clear();
-	move(TITLE_LINE, TITLE_POS);		//gotoxy( TITLE_POS, TITLE_LINE );
-	printw( "住所管理表    第一版\n%s", LINE);
+	move(TITLE_LINE, TITLE_POS);
+	printw( "住所管理表    第二版\n%s", LINE);
 }
 
 
@@ -391,7 +565,7 @@ void show_one_page( void )
 
 	/* 前処理 */
 	for( i = DATA_START ; i<= DATA_END ; i++ ){
-		move(i, 0);
+		move(i, 1);
 		clrtoeol();
 	}
 	start = ((( cursor -1 ) / 5)*5 + 1);
@@ -426,7 +600,31 @@ void	show_record(
 		printw( " " );
 
 	idx = num-1;
-	printw("%3d %-20s TEL %-15s \n" ,num, j_name[idx], tel_no[idx] );
+	printw("%3d %-20s ℡ %-15s \n" ,num, j_name[idx], tel_no[idx] );
 	printw("     %-20s 〒%-8s %s\n", k_name[idx], zip[idx], address[idx] );
 	printw( "%s", LINE );
+}
+
+
+/* del_lines */
+/*----------------------------------------------------*/
+/*
+	書式：	void	del_lines( int start, int end )
+
+	機能：	引数,start,endで指定された行を消す
+
+	返値：	なし
+
+*/
+
+void	del_lines( start, end )
+int		start, end;
+{
+	int		i;
+
+	move(start, 1);
+	for( i = start ; i <= end; i++ ){
+		clrtoeol();
+		addch( '\n' );
+	}
 }
